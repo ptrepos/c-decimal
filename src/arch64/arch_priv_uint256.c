41a731e55bd9094e09e30c43b806463d99a68764
takuo hada <t_hada@dachicraft.net>
2016-08-24 22:44:30 +0900

    mg_uint256_divの無駄な処理を見直し

--- src/arch64/arch_priv_uint256.c
+++ src/arch64/arch_priv_uint256.c
@@ -166,32 +166,28 @@ MG_PRIVATE mg_decimal_error mg_uint256_div(mg_uint256 *op1, const mg_uint256 *op
 	mg_uint256 *q = &buf1, *qv = &buf2, *qv_hi = &buf3;
 	int q_n, underflow;
 
-	if(mg_uint256_is_zero(op2)) {
-		err = MG_DECIMAL_ERROR_ZERODIVIDE;
-		goto _ERROR;
-	}
-
 	int op1_digits = MG_UINT256_WORD_COUNT;
 	while(op1_digits > 0 && op1->word[op1_digits-1] == 0)
 		op1_digits--;
 	int op2_digits = MG_UINT256_WORD_COUNT;
 	while(op2_digits > 0 && op2->word[op2_digits-1] == 0)
 		op2_digits--;
-	
-	if(op1_digits <= 1 && op2_digits <= 1) {
-		mg_uint256_set(quotient, op1->word[0] / op2->word[0]);
-		mg_uint256_set(op1, op1->word[0] % op2->word[0]);
-		goto _EXIT;
+
+	if(op2_digits <= 1) {
+		if(op2_digits <= 0) {
+			err = MG_DECIMAL_ERROR_ZERODIVIDE;
+			goto _ERROR;
+		}
+		if(op1_digits <= 1) {
+			mg_uint256_set(quotient, op1->word[0] / op2->word[0]);
+			mg_uint256_set(op1, op1->word[0] % op2->word[0]);
+			goto _EXIT;
+		}
 	}
 	
 	max_float_t op2_v = (max_float_t)op2->word[op2_digits -1];
 	if(op2_digits >= 2)
 		op2_v += (max_float_t)op2->word[op2_digits-2] * DOUBLE_RSHIFT_64;
-
-	if(op2_v == 0.0) {
-		err = MG_DECIMAL_ERROR_ZERODIVIDE;
-		goto _ERROR;
-	}
 
 	mg_uint256_set_zero(/*out*/quotient);
 
